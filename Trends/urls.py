766a946e694ac7c283fa133d182029725a63661f
nandishpatel1996 <1418beit30031@gmail.com>
2018-06-01 22:29:39 +0530

    getting tweets done

--- Trends/urls.py
+++ Trends/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.trends,name='trends'),
+    url(r'hashtag=\s*', views.hashtag_details,name='hashtag_details'),
 ]
--- Trends/views.py
+++ Trends/views.py
@@ -13,8 +13,8 @@ auth.set_access_token(atoken, asecret)
 api = tweepy.API(auth)
 
 def trends(request):
+    print(request.GET)
     trends_list = api.trends_place(1)
-    #print(trends_list)
     trends_list = trends_list[0]['trends']
     list=[]
     for i in trends_list:
@@ -23,3 +23,37 @@ def trends(request):
         list.append(i['name'])
     template = loader.get_template('list_trends.html')
     return HttpResponse(template.render({'trends':list}, request))
+
+class TweetStreamListener(tweepy.StreamListener):
+
+    def __init__(self,api=None):
+        super(TweetStreamListener,self).__init__()
+        self.cnt = 1
+        self.end = 5
+        print("started")
+
+    def on_status(self, status):
+        if self.end >= self.cnt:
+            print(status._json['text'])
+            print(type(status._json['text']))
+            self.cnt+=1
+        else:
+            print('end')
+            return False
+
+    def on_error(self, status_code):
+        print("error", status_code)
+        return False
+
+    def on_timeout(self):
+        print('timeout')
+        return False
+
+
+def hashtag_details(request):
+    streamListener = TweetStreamListener()
+    myStream = tweepy.Stream(auth=api.auth,listener=streamListener)
+    hashtag = ((request.path[1:]).split('='))[1]
+    myStream.filter(track=hashtag,async=True)
+
+    return HttpResponse("<h1>hashtag</h1>")
